687ffae598de432d0210d9e0d3a80ff77658bc35
Jamie Cerexhe <jcerexhe@gmail.com>
2017-04-04 14:21:33 +1000

    added stripe integration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,7 @@ class PagesController < ApplicationController
 
   def guest_dashboard
   	@bookings = current_user.bookings
+  	@approved_bookings = @bookings.approved
+  	@pending_bookings = @bookings.pending
   end
 end
